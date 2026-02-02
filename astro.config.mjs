35ef4934113cccd48c7a003be09e7a14ead07c5c
gofooditalia <italiagofood@gmail.com>
2026-02-02 13:31:07 +0100

    feat: centralize menu redirect to /menu and update status to 302

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  redirects: {
+    '/menu': {
+      destination: 'https://gofoodmenu.it/osteria-cilea',
+      status: 302
+    }
+  },
   vite: {
     plugins: [tailwindcss()]
   }
